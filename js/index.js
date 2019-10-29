e04ad6ee37a142341b2bdf487a5ec9f7a35e8ff1
Apiyo4 <odingoannette@gmail.com>
2019-10-29 17:06:43 +0300

    Work on 4 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // Your code goes here
+const newP = document.createElement('p');
+const navContainer = document.querySelector('.nav-container');
+newP.textContent = 'You can remove Fun Bus'
+navContainer.prepend(newP);
+//1.mouseover
+const navE = document.querySelector('.nav');
+navE.addEventListener('mouseover', (event)=>{
+    event.target.style.color = 'blue';
+    setTimeout(() => {
+        event.target.style.color = "";
+    }, 500);
+    
+    console.log("mouse has moved to next nav element");
+    console.log(event);
+});
+//2.drag
+const h1Header = document.querySelector('header h1');
+h1Header.addEventListener('drag', (event)=>{
+    console.log("Fun Bus is being drag");
+    console.log(event.target);
+   newP.textContent = 'Fun bus was removed';
+   h1Header.style.display = 'none'; 
+    
+})
+//3.click
+const bigBtn = document.querySelectorAll('.btn');
+for(let i= 0; i<bigBtn.length; i++ ){
+bigBtn[i].addEventListener('click', (event) => {
+    const newPa = document.createElement('p');
+    newPa.textContent = "You've clicked me";
+    const classDestination = document.querySelector('.destination');
+    classDestination.append(newPa);
+})
+} 
+
+// 4.dblClick
+const introP = document.querySelector('.intro p');
+
+introP.addEventListener('dblClick', (event)=>{
+    event.target.style.color = 'red';
+   
+})
+
